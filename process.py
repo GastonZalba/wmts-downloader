336e81b101a2482a425691c99b52170ba99ba050
Gastón Zalba <gastonzalba@outlook.com>
2022-06-17 19:37:05 -0300

    Basic functions are working. Added argparse

--- process.py
+++ process.py
@@ -1,100 +1,218 @@
 import os
-import requests
-from requests.models import PreparedRequest
-from params import *
-import xmltodict
+import shutil
 import traceback
+import tempfile
+import argparse
 from colorama import init, Fore, Style
 from owslib.wmts import WebMapTileService
-import shutil
 
 # fix colorama colors in windows console
 init(convert=True)
 
-try:
+tmp_folder = f'{tempfile.gettempdir()}\\wmts-downloader'
+output_folder = 'output'
 
-    # check if output folder exists
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+zoom = 15
+format = 'image/png'
+url = ''
+proj = 'EPSG:3857'
 
-    for d in dw:
+parser = argparse.ArgumentParser(description='Script to download images from a WMTS service')
+parser.add_argument('url', type=str, metavar='WMTS server url', help='Server url (default: %(default)s)')
+parser.add_argument('--layer', type=str, metavar='Layer name', help='Layer name (default: %(default)s)')
+parser.add_argument('--format', type=str, metavar='Image format', default=format, help='Image format supported by the geoserver (default: %(default)s)')
+parser.add_argument('--zoom', type=int, metavar='Zoom level', default=zoom, help='Zoom level. Higher number is more detail, and more images (default: %(default)s)')
+parser.add_argument('--proj', type=str, metavar='EPSG projection code', default=proj, help='EPSG projection code existing in the geoserver (default: %(default)s)')
+parser.add_argument('--output', type=str, metavar='Output folder', default=output_folder, help='Folder to save the images (default: %(default)s)')
 
-        url = d['url']
+args = parser.parse_args()
 
-        version = d['version'] if 'version' in d.keys() else '1.3.0'
+def init():
 
-        params = {
-            'version': version,
-            'service': 'WMS',
-            'request': 'GetCapabilities'
-        }
+    global output_folder
 
-        req = PreparedRequest()
-        req.prepare_url(url, params)
+    try:
 
-        resp = requests.get(req.url)
+        print('--> PROCESS STARTED <--')
 
-        xml = xmltodict.parse(resp.content)
+        print('\t')
 
-        layers = xml['Capabilities']['Contents']['Layer']
+        # check if output folder exists
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
 
-        format = d['format']
+        if not os.path.exists(tmp_folder):
+            os.makedirs(tmp_folder)
 
-        zoom = d['zoom']
+        url = args.url
+        format = args.format
+        zoom = int(args.zoom)
+        proj = args.proj
+        layer_id = args.layer 
+        output_folder = args.output
 
-        proj =  d['epsg']
+        download_count = 1
+              
+        print(f'Connecting to server: {url}')
 
-        layer_id = d['layer']
+        try:
+            wmts = WebMapTileService(url)
+        except Exception as error:
+            print(f"{Fore.RED}-> Can't connect to server{Style.RESET_ALL}")
+            print(f'{Fore.RED}--> PROCESS WAS ABORTED WITH ERRORS <--{Style.RESET_ALL}')
+            return
 
-        for layer in layers:
-            if layer['ows:Identifier'] == d['layer']:
-                title = layer["ows:Title"]
-                abstract = layer["ows:Abstract"]
-                formats = layer["Format"]
-                tile_matrixs = layer['TileMatrixSetLink']
-                for tm in tile_matrixs:
+        print(f'{Fore.GREEN}-> Connection successful{Style.RESET_ALL}')
+        print(f'-> Title: {wmts.identification.title}')
+        print(f'-> Access constraints: {wmts.identification.accessconstraints}')
 
-                    tile_matrix_set = tm['TileMatrixSet']
+        contents = wmts.contents
+
+        print('\t')
+
+        for layer in wmts.contents:
+            layer = contents[layer]
+
+            if layer.id == layer_id:
+
+                print(f'-> Layer {layer_id} found')
+
+                title = layer.title
+                abstract = layer.abstract
+                extent = layer.boundingBoxWGS84
+                formats = layer.formats
+
+                print(f'--> Title: {title}')
+                print(f'--> Abstract: {abstract}')
+                print(f'--> Bounding Box WGS84: {extent}')
+                print(f'--> Available formats: {formats}')
+
+                tile_matrixs_links = layer.tilematrixsetlinks
+
+                print(f'--> Available tile matrix sets: {layer._tilematrixsets}')
+
+                for tile_matrix_set in tile_matrixs_links:
 
                     if tile_matrix_set == proj:
 
-                        tile_matrix_limits = tm['TileMatrixSetLimits']['TileMatrixLimits']
+                        tile_matrix_link = tile_matrixs_links[tile_matrix_set]
 
-                        for tml in tile_matrix_limits:
+                        tile_matrix = wmts.tilematrixsets[tile_matrix_set].tilematrix
 
-                            z = int(tml['TileMatrix'].split(":")[-1])
+                        for tml in tile_matrix:
+
+                            z = int(tml.split(":")[-1])
 
                             if z == zoom:
                                 limit = tml
-                        
+
+                        matrix_limits = tile_matrix_link.tilematrixlimits[limit]
+
+                        # important
+                        matrix = tile_matrix[limit]
+
+                        min_row = matrix_limits.mintilerow
+                        max_row = matrix_limits.maxtilerow
+
+                        min_col = matrix_limits.mintilecol
+                        max_col = matrix_limits.maxtilecol
+
                         # check if output folder exists
-                        layer_folder = f'{output_folder}\\{layer_id}'
+                        output_folder = f'{output_folder}\\{layer_id}'
 
-                        if os.path.exists(layer_folder):
-                            shutil.rmtree(layer_folder)
+                        if os.path.exists(output_folder):
+                            shutil.rmtree(output_folder)
+
+                        os.makedirs(output_folder)
                         
-                        os.makedirs(layer_folder)
+                        print('\t')
+                        print('Downloading images...')
 
-                        wmts = WebMapTileService(url)
-                        
-                        tile_matrix = limit['TileMatrix']
-                        row = limit['MinTileRow']
-                        col = limit['MinTileCol']
+                        for row in range(min_row, max_row):
 
-                        img = wmts.gettile(
-                            url,
-                            layer=layer_id,
-                            tilematrixset=tile_matrix_set,
-                            tilematrix=tile_matrix,
-                            row=row,
-                            column=col,
-                            format=format
-                        )                        
+                            for col in range(min_col, max_col):
 
-                        out = open(f'{layer_folder}\\{layer_id}_zoom-{zoom}_col-{col}_row-{row}.png', 'wb')
-                        bytes_written = out.write(img.read())
-                        out.close()
+                                print(f'--> Downloading tile ({download_count}): Column {col} - Row {row} - Zoom {zoom}')
 
-except Exception as error:
-    print(f'{Fore.RED}{error}{Style.RESET_ALL}')
-    print(traceback.format_exc())
+                                img = wmts.gettile(
+                                    url,
+                                    layer=layer_id,
+                                    tilematrixset=tile_matrix_set,
+                                    tilematrix=limit,
+                                    row=row,
+                                    column=col,
+                                    format=format
+                                )
+
+                                extension = format.split("/")[-1]
+
+                                file_name = f'{layer_id}__{proj.replace(":", "-")}_col-{col}_row-{row}_zoom-{zoom}'
+
+                                write_world_file(file_name, extension, col, row, matrix)
+                                
+                                write_image(file_name, extension, img)
+
+                                download_count += 1
+        
+        
+        print('\t')
+        print('--> PROCESS WAS COMPLETED <--')
+        print('------------------------------')
+        print(f'-> Format: {format}')
+        print(f'-> Projection: {proj}')
+        print(f'-> Zoom: {zoom}')
+
+        if download_count:
+            print(f'{Fore.GREEN}-> Downloaded files: {download_count}{Style.RESET_ALL}')
+        else:
+            print(f'{Fore.YELLOW}-> No files downloaded{Style.RESET_ALL}')
+
+        print('------------------------------')
+
+        if os.path.exists(tmp_folder):
+            shutil.rmtree(tmp_folder)
+
+    except Exception as error:
+        print(f'{Fore.RED}{error}{Style.RESET_ALL}')
+        print(traceback.format_exc())
+
+
+def write_image(file_name, extension, img):
+    '''
+    Writes images
+    '''
+    file_path = f'{output_folder}\\{file_name}.{extension}'
+
+    out = open(file_path, 'wb')
+    out.write(img.read())
+    out.close()
+
+
+def write_world_file(file_name, extension, col, row, matrix):
+    '''
+    Writes world file
+    https://gdal.org/drivers/raster/wld.html
+    https://en.wikipedia.org/wiki/World_file
+    '''
+
+    if extension == 'png':
+        wf_ext = 'pgw'
+    elif extension == 'tiff' or extension == 'tiff':
+        wf_ext = 'tfw'
+    elif extension == 'jpg' or extension == 'jpeg':
+        wf_ext = 'jgw'
+    elif extension == 'gif':
+        wf_ext = 'gfw'
+    else:
+        wf_ext = 'wld'
+
+    pixel_size = 0.00028 # Each pixel is assumed to be 0.28mm
+    a = matrix.scaledenominator * pixel_size
+    e = matrix.scaledenominator * -pixel_size
+    left = ((col * matrix.tilewidth + 0.5) * a) + matrix.topleftcorner[0]
+    top = ((row * matrix.tileheight + 0.5) * e) + matrix.topleftcorner[1]
+
+    with open(f'{output_folder}\\{file_name}.{wf_ext}', 'w') as f:
+        f.write('%f\n%d\n%d\n%f\n%f\n%f' % (a, 0, 0, e, left, top))
+
+init()
