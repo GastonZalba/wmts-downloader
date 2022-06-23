22b036b1d14bcd19566583f673da6d65c1bc379e
Gastón Zalba <gastonzalba@outlook.com>
2022-06-23 16:23:02 -0300

    Fixed combine.py (WIP)

--- combine.py
+++ combine.py
@@ -3,7 +3,8 @@ import glob
 import rasterio
 from rasterio.mask import mask
 from rasterio.merge import merge
-from shapely.geometry import Polygon
+from shapely.geometry import box
+
 import traceback
 import geopandas as gpd
 from colorama import init, Fore, Style
@@ -17,37 +18,39 @@ gdf_cards = gpd.read_file('cartas.geojson')
 
 def init():
     try:
-        global output_folder
+        global output_folder, collect_path_images
 
         images = get_images()
 
         for index, carta in gdf_cards.iterrows():
-            id_carta = carta['carac']
-            faja = carta['num_faja']
-            geom = carta['geometry']
+            id_carta = carta['caracteristica_de_hoja']
+            faja = carta['numero_faja']
+            geom = carta.geometry
 
             print('-> Proccessing', id_carta, faja)
-            
+
             collect_path_images = []
 
             for image in images:
                 file_name = os.path.basename(image)
                 layer_name = file_name.split('__')[0]
                 attributes = file_name.split('_')
-                crs = [x for x in attributes if 'EPSG' in x][0].replace('-', ':')
+                crs = [x for x in attributes if 'EPSG' in x][0].replace(
+                    '-', ':')
 
                 with rasterio.open(image, crs=crs) as src:
-                    bounds = src.bounds
-                    bounds = bbox(bounds.left, bounds.bottom, bounds.right, bounds.top)
-                    gdf_image = gpd.GeoDataFrame(index=[0], crs=crs, geometry=[bounds]).iloc[0]
-                   
-                    if geom.intersects(gdf_image['geometry']):
+                    bounds_image = box(*src.bounds)
+
+                    if bounds_image.intersects(geom):
                         collect_path_images.append(image)
                         print(f'-> Matched image {file_name} with {id_carta}')
-                    else:
-                        print(f'-> Unmatched image {file_name} with {id_carta}')
-            
+                        print(bounds_image)
+                        print(geom)
+                    #else:
+                        #print(f'-> Unmatched image {file_name} with {id_carta}')
+
             if len(collect_path_images) < 1:
+                print('--> Skip')
                 continue
 
             output_folder_layer = f'{output_folder}/{layer_name}'
@@ -57,13 +60,29 @@ def init():
             if not os.path.exists(output_folder_layer_crs):
                 os.makedirs(output_folder_layer_crs)
 
+            file_tmp = f'{output_folder_layer_crs}/{id_carta}_tmp.tif'
+
             # merge collected
-            mosaic = merge(collect_path_images, indexes=[1,2], dst_path=f'{output_folder_layer_crs}/{id_carta}.tif')
+            merge(collect_path_images, indexes=[1, 2], dst_path=file_tmp)
+            
+            print(file_tmp, faja)
+            print(geom)
+            print(collect_path_images)
 
-            print(f'-> Merged tiles {mosaic}')
+            with rasterio.open(file_tmp) as src:
+                out_image, out_transform = mask(src, [geom], crop=True)
+                out_meta = src.meta
 
-            # crop 
-            # out_image, out_transform = mask(merged, geom, crop=True)
+            # crop
+            out_meta.update({
+                "driver": "GTiff",
+                "height": out_image.shape[1],
+                "width": out_image.shape[2],
+                "transform": out_transform
+            })
+
+            with rasterio.open(f'{output_folder_layer_crs}/{id_carta}.tif', "w", **out_meta) as dest:
+                dest.write(out_image)
 
             # save 3857
 
@@ -74,18 +93,11 @@ def init():
                 dst_crs = 'EPSG:5347'
             elif faja == 6:
                 dst_crs = 'EPSG:5348'
-        
 
     except Exception as error:
         print(f'{Fore.RED}{error}{Style.RESET_ALL}')
         print(traceback.format_exc())
 
-# function to return polygon
-def bbox(long0, lat0, lat1, long1):
-    return Polygon([[long0, lat0],
-                    [long1,lat0],
-                    [long1,lat1],
-                    [long0, lat1]])
 
 def get_images():
     types = ('*.png', '*.jpg', '*.jpeg', '*.tiff', '*.tif')
@@ -93,5 +105,6 @@ def get_images():
     for type in types:
         files_grabbed.extend(glob.glob(f'{input_folder}/{type}'))
     return files_grabbed
+
 
 init()
